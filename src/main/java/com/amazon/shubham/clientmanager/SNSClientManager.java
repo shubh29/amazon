b12783d24bf78db08e9954c46fc4cc1328ed6afb
Shubham Mahajan <shubham.mahajan@icloud.com>
2018-04-22 15:01:58 -0400

    after implementing sms validation

--- src/main/java/com/amazon/shubham/clientmanager/SNSClientManager.java
+++ src/main/java/com/amazon/shubham/clientmanager/SNSClientManager.java
@@ -1,5 +1,7 @@
 package com.amazon.shubham.clientmanager;
 
+import org.apache.log4j.Logger;
+
 import com.amazonaws.auth.ClasspathPropertiesFileCredentialsProvider;
 import com.amazonaws.regions.Region;
 import com.amazonaws.regions.Regions;
@@ -8,27 +10,29 @@ import com.amazonaws.services.sns.AmazonSNSClient;
 //Singleton pattern
 //Create only object of SNSClientManager
 public class SNSClientManager {
-  
-  //Eager loading
-  private static SNSClientManager snsClientManager = new SNSClientManager();
-  
-  private AmazonSNSClient snsClient;
-  
-  //Make the constructor private so that this class cannot be instantiated
-  protected SNSClientManager() {
-    System.out.println("Calling SNS Client");
-    snsClient = new AmazonSNSClient(new ClasspathPropertiesFileCredentialsProvider());		                           
-    snsClient.setRegion(Region.getRegion(Regions.US_EAST_1));
-  }
 
-  //Get the only object available
-  public static SNSClientManager getInstance() {
-    return snsClientManager;
-  }
+	private static Logger sLogger = Logger.getLogger(SNSClientManager.class);
 
-  //Get SNSClient
-  public AmazonSNSClient getAmazonSNSClient() {
-  // TODO Auto-generated method stub
-  return snsClient;
-  }
+	// Eager loading
+	private static SNSClientManager snsClientManager = new SNSClientManager();
+
+	private AmazonSNSClient snsClient;
+
+	// Make the constructor private so that this class cannot be instantiated
+	protected SNSClientManager() {
+		sLogger.debug("Calling SNS Client");
+		snsClient = new AmazonSNSClient(new ClasspathPropertiesFileCredentialsProvider());
+		snsClient.setRegion(Region.getRegion(Regions.US_EAST_1));
+    }
+
+	// Get the only object available
+	public static SNSClientManager getInstance() {
+		return snsClientManager;
+	}
+
+	// Get SNSClient
+	public AmazonSNSClient getAmazonSNSClient() {
+		// TODO Auto-generated method stub
+		return snsClient;
+	}
 }
--- src/main/java/com/amazon/shubham/subscription/SMSSubscription.java
+++ src/main/java/com/amazon/shubham/subscription/SMSSubscription.java
@@ -1,22 +1,29 @@
 package com.amazon.shubham.subscription;
 
+import org.apache.log4j.Logger;
 
 public class SMSSubscription extends Subscription {
 
-  @Override
-  public boolean validate(String value) {
-  // TODO Auto-generated method stub
-  //if(value.matches("\\d{3}[-\\.\\s]\\d{3}[-\\.\\s]\\d{4}"))
-  return true;
-//else
-//	System.err.println("Please enter value in the format:123-456-7890");
-//	return false;
-  }
+	private static Logger sLogger = Logger.getLogger("com.amazon.shubham.validation");
 
-  @Override
-  public String getType() {
-    // TODO Auto-generated method stub
-    return "sms";
-  }
+	@Override
+	public boolean validate(String value) {
+		// TODO Auto-generated method stub
+		if (value.matches("[1][-]\\d{3}[-]\\d{3}[-]\\d{4}")) {
+			sLogger.info("SMS number validated. Number is : " + value);
+			return true;
+		} else
+		{
+			System.err.println("Please enter value in the format:1-123-456-7890");
+			sLogger.error("SMS number invalid. Number is : " + value);
+		}
+		return false;
+	}
+
+	@Override
+	public String getType() {
+		// TODO Auto-generated method stub
+		return "sms";
+	}
 
 }
