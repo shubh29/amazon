31e004780e3fda0ec55de4c7ee6aa23e5576d062
Shubham Mahajan <shubham.mahajan@icloud.com>
2018-04-25 20:45:10 -0400

    After adding assembly plugin

--- src/main/java/com/amazon/shubham/subscription/SMSSubscription.java
+++ src/main/java/com/amazon/shubham/subscription/SMSSubscription.java
@@ -3,27 +3,27 @@ package com.amazon.shubham.subscription;
 import org.apache.log4j.Logger;
 
 public class SMSSubscription extends Subscription {
+	
+  private static Logger sLogger = Logger.getLogger("com.amazon.shubham.validation");
 
-	private static Logger sLogger = Logger.getLogger("com.amazon.shubham.validation");
-
-	@Override
-	public boolean validate(String value) {
-		// TODO Auto-generated method stub
-		if (value.matches("[1][-]\\d{3}[-]\\d{3}[-]\\d{4}")) {
-			sLogger.info("SMS number validated. Number is : " + value);
-			return true;
-		} else
-		{
-			System.err.println("Please enter value in the format:1-123-456-7890");
-			sLogger.error("SMS number invalid. Number is : " + value);
-		}
-		return false;
+  @Override
+  //validate if the contact number is in valid format
+  public boolean validate(String value) {
+    // TODO Auto-generated method stub
+    if (value.matches("[1][-]\\d{3}[-]\\d{3}[-]\\d{4}")) {
+      sLogger.info("SMS number validated. Number is : " + value);
+      return true;
+    } else {
+      System.err.println("Please enter value in the format:1-123-456-7890");
+      sLogger.error("SMS number invalid. Number is : " + value);
 	}
+	return false;
+  }
 
-	@Override
-	public String getType() {
-		// TODO Auto-generated method stub
-		return "sms";
-	}
+  @Override
+  public String getType() {
+    // TODO Auto-generated method stub
+    return "sms";
+  }
 
 }
