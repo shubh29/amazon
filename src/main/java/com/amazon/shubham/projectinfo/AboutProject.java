0bec4f5111cda8d3065a7aadccfea43df739827a
Shubham Mahajan <shubham.mahajan@icloud.com>
2018-05-10 14:33:32 -0400

    demo change

--- src/main/java/com/amazon/shubham/projectinfo/AboutProject.java
+++ src/main/java/com/amazon/shubham/projectinfo/AboutProject.java
@@ -6,7 +6,7 @@ public class AboutProject {
 		System.out.println("This is a snall java console application that uses Amazon's Simple Notification Service");
 		System.out.println("The program interface will allow the user to select the basic operations on SNS by programatic pauses e.g. Press return to continue ");
 		System.out.println("The project is developed solely for the consumers of Independent Study");
-		System.out.println("The operations are as follows:");
+		System.out.println("The operations are as follows");
 		System.out.println("Create Topic, List Topic, DeleteTopic, Subscribe to a topic via email/sms, List Subscribtions, Unsubscribe, and Publish");
 	}
 }
